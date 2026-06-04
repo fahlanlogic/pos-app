85b177502489a5e2cfd492563b8a85008ff903aa
fahlanlogic <alanfahdi@gmail.com>
2026-06-05 02:04:03 +0700

    feat: crlf to lf

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Button } from "./components/ui/Button";
 function App() {
   return (
     <div className="bg-white dark:bg-black text-black dark:text-white min-h-screen">
-      <Button>Press me!</Button>
+      <Button>Press me</Button>
     </div>
   );
 }
