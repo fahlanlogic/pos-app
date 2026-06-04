1c206448dd7bb677cddf935c8ab48f604eb95e71
fahlanlogic <alanfahdi@gmail.com>
2026-06-05 01:06:04 +0700

    feat: test add react-aria-component Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import { Button } from "./components/ui/Button";
 
 function App() {
   return (
     <div className="bg-white dark:bg-black text-black dark:text-white min-h-screen">
-      Tes husky
+      <Button>Press me</Button>
     </div>
   );
 }
