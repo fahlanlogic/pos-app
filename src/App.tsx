b30ca17797cec17c7b3ec0748ea84268ac99cf3f
fahlanlogic <alanfahdi@gmail.com>
2026-06-04 23:10:11 +0700

    feat: test husky lint-staged

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-white dark:bg-black text-black dark:text-white min-h-screen">
-      Tes
+      Tes husky
     </div>
   );
 }
